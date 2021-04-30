74773ae15f51a42e7873783f0459315b076f68de
KimNam-Hyong <kimnamhyong@gmail.com>
2021-04-30 13:11:27 +0900

    config

--- config/config.js
+++ config/config.js
@@ -2,28 +2,31 @@ require('dotenv').config();
 
 module.exports = {
     development:{
-        username:'root',
+        username:'dbmasteruser',
         password:process.env.SEQUELIZE_PASSWORD,
         database:'community',
-        host:'127.0.0.1',
-        dialect:'mysql',
-        timezone: '+09:00'
-    },
-    test:{
-        username:'root',
-        password:process.env.SEQUELIZE_PASSWORD,
-        database:'community',
-        host:'127.0.0.1',
-        dialect:'mysql',
-        timezone: '+09:00'
-    },
-    production:{
-        username:'root',
-        password:process.env.SEQUELIZE_PASSWORD,
-        database:'community',
-        host:'127.0.0.1',
+        host:'ls-6a04beaafd7004c6bc227db32499ab66d9e75442.cobixnpefx6y.ap-northeast-2.rds.amazonaws.com',
         dialect:'mysql',
         timezone: '+09:00',
-        logging:false
+        timestamps:true
+    },
+    test:{
+        username:'dbmasteruser',
+        password:process.env.SEQUELIZE_PASSWORD,
+        database:'community',
+        host:'ls-6a04beaafd7004c6bc227db32499ab66d9e75442.cobixnpefx6y.ap-northeast-2.rds.amazonaws.com',
+        dialect:'mysql',
+        timezone: '+09:00',
+        timestamps:true
+    },
+    production:{
+        username:'dbmasteruser',
+        password:process.env.SEQUELIZE_PASSWORD,
+        database:'community',
+        host:'ls-6a04beaafd7004c6bc227db32499ab66d9e75442.cobixnpefx6y.ap-northeast-2.rds.amazonaws.com',
+        dialect:'mysql',
+        timezone: '+09:00',
+        logging:false,
+        timestamps:true
     }
 }
